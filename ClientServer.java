7e654b46f939e53ed3874c289b70e7a8e50851c3
tiagoviniciuslopes <tiago.pereira@eits.com.br>
2021-03-30 12:05:49 -0300

    criando repositorio

---
+++ ClientServer.java
@@ -0,0 +1,92 @@
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.OutputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class ClientServer implements Runnable{
+
+	Client client;
+	ServerSocket receiver;
+	
+	public ClientServer(Client client) throws Exception{
+		this.client = client;
+		receiver = new ServerSocket(8080);
+	}
+
+	//Thread que aceita conexao do servidor para receber arquivos
+	public void run () {
+		while(true){
+			try{
+				//Aceita a conexao com o servidor
+				Socket cliente = receiver.accept();
+				if(cliente.isBound()){
+					receiveFile(cliente); // Recebe arquivos do servidor
+				}
+			}catch(IOException e){
+				System.out.println("catch accept: " + e.getMessage());
+			}
+		}
+	}
+	
+	public void receiveFile(Socket socket){
+		try{
+			int bytesRead;  
+			int current = 0;  
+				   
+			InputStream in = socket.getInputStream();
+			DataInputStream socketData = new DataInputStream(in);   
+			String fileName = socketData.readUTF();     
+			long size = socketData.readLong();
+			long sizeAux = size;
+			long startTime = System.currentTimeMillis();
+			
+			
+			if(size!=0L){
+				OutputStream output = new FileOutputStream("C:\\ftp_received\\"+fileName); 
+				byte[] buffer = new byte[1024];     
+				
+				while (size > 0 && (bytesRead = socketData.read(buffer, 0, (int)Math.min(buffer.length, size))) != -1)     
+				{     
+					output.write(buffer, 0, bytesRead);     
+					size -= bytesRead;
+					
+					System.out.println("Arquivo " + fileName + " " + (((sizeAux-size)*100)/sizeAux) + "% recebido ");  
+				}  
+				
+				double estimatedTime = (System.currentTimeMillis() - startTime)/1000;
+
+				for (int i = 0; i < 100; ++i) System.out.println();
+				System.out.println("Arquivo " + fileName + " recebido!");
+				System.out.println("Arquivos recebidos ficam na pasta C:\\ftp_received");
+				
+				if(estimatedTime!=0){
+					System.out.println("Taxa de transferencia: " + (((double)sizeAux)/1000)/estimatedTime + "kbps");   
+					System.out.println("Tamanho: " + (((double)sizeAux)/1000) + "kb \nTempo: " + estimatedTime + " segundos");
+				}else{
+					System.out.println("Taxa de transferencia: " + sizeAux + "bps");
+					System.out.println("Tamanho: " + (sizeAux) + "bytes \nTempo: 1 segundo");  
+				}
+				
+				
+				output.close();
+			}else{
+				System.out.println(fileName);
+			}		
+		}catch(IOException e){
+			System.out.println("catch receive: " + e.getMessage());
+		}
+	}
+}
---
+++ Server.java
@@ -0,0 +1,103 @@
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.OutputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.File;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.file.*;
+
+public class Server{
+	
+	
+	ServerSocket servidor;
+	ArrayList<Socket> clientes= new ArrayList<Socket>();
+	
+	
+
+	
+	public static void main(String[] args) throws Exception{
+		Server r = new Server();
+		
+		r.abreServidor();
+		r.executa();
+	}
+	
+	public void abreServidor() throws Exception{
+		servidor = new ServerSocket(8889);
+		InetAddress ip = InetAddress.getLocalHost();	
+		new File("C:\\ftp").mkdir(); //Pasta onde os arquivos recebidos serão armazenados
+		System.out.println("Porta "+servidor.getLocalPort()+" aberta!");
+		System.out.println("IP do servidor: "+ip.getHostAddress());
+		
+	}
+	
+	public void executa() throws IOException{
+		
+		while(true){
+			try{
+				//Aceita um cliente
+				Socket cliente = servidor.accept();
+				Socket aux=null;
+				
+				for (Socket s:clientes) {
+					if(s.getInetAddress().getHostAddress().equals(cliente.getInetAddress().getHostAddress())){
+						aux = s;
+					}
+				}
+				clientes.remove(aux);
+				clientes.add(cliente);
+				
+				System.out.println("\nCliente conectado: " + cliente.getInetAddress().getHostAddress());
+				if(cliente.isBound()){
+					ServerReceiver rc = new ServerReceiver(cliente, this);
+					new Thread(rc).start();
+				}
+			}catch(IOException e){
+				System.out.println("catch accept: " + e.getMessage());
+			}
+		}
+
+
+	}
+	
+	public void sendUsers(String ip){
+		
+
+		try{
+			String line="";
+
+			for (Socket s:clientes) {
+				if(s.isBound() && s.isConnected() && !s.isClosed() && !s.isInputShutdown() && !s.isOutputShutdown()){
+					line =  line.concat(s.getInetAddress().getHostAddress()+System.getProperty("line.separator"));
+				}
+			}
+
+			Socket sender = new Socket(ip,8080);
+			
+			OutputStream os = sender.getOutputStream();   
+			
+			DataOutputStream dos = new DataOutputStream(os);     
+			dos.writeUTF(line); //Enviando o nome do arquivo
+			dos.writeLong(0L); //Enviando o tamanho do arquivo como 0 pra sinalizar que eh get    
+			dos.flush(); //Forca o envio		 
+			
+			   
+			sender.close();
+			sender = null;
+		}catch(IOException e){
+			System.out.println("catch sendlist: " + e.getMessage());
+		}
+	}
+}
